7e2011da7db28f1816b46a99db69bf906692b7d6
Reshma Bhatia <reshmabhatian@gmail.com>
2016-11-22 02:34:47 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>ShopUsingBitcoin</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<!--[if IE 6]>
+<link rel="stylesheet" type="text/css" href="iecss.css" />
+<![endif]-->
+<script type="text/javascript" src="js/boxOver.js"></script>
+</head>
+<body>
+<div id="main_container">
+  <div class="top_bar">
+  <div id="header">
+    <div id="logo"> <a href="index1.html"><img src="images\index.jpg" alt="" border="0" width="237" height="140" /></a> </div>
+    <div class="oferte_content">
+      <div class="top_divider"><img src="images/header_divider.png" alt="" width="1" height="164" /></div>
+      <div class="oferta">
+        <div class="oferta_content"> <img src="images/laptop.png" width="94" height="92" alt="" border="0" class="oferta_img" />
+          <div class="oferta_details">
+            <div class="oferta_title">Seminar Project</div>
+            <div class="oferta_text"> Our seminar project aims at creating an online shopping scenario with the help of bitcoins :D No need for any cash/card transactions . Purchase via only BIT COINS</div>
+            <a href="details.html" class="details">details</a> </div>
+        </div>
+        <div class="oferta_pagination"> <span class="current">1</span> <a href="details2.html">2</a> <a href="details3.html">3</a> <a href="details4.html">4</a> <a href="details5.html">5</a> </div>
+      </div>
+      <div class="top_divider"><img src="images/header_divider.png" alt="" width="1" height="164" /></div>
+    </div>
+    <!-- end of oferte_content-->
+  </div>
+  <div id="main_content">
+    <div id="menu_tab">
+      <div class="left_menu_corner"></div>
+      <ul class="menu">
+        <li><a href="accesory wala address" class="nav1" > Home</a></li>
+        <li class="divider"></li>
+        <li><a href="#" class="nav2">Products</a></li>
+        <li class="divider"></li>
+        <li><a href="#" class="nav3">Specials</a></li>
+        <li class="divider"></li>
+        <li><a href="#" class="nav4">My account</a></li>
+        <li class="divider"></li>
+        <li><a href="#" class="nav5">Sign Up</a></li>
+        <li class="divider"></li>
+        <li><a href="@" class="nav5">Shipping</a></li>
+        <li class="divider"></li>
+        <li><a href="contact.html" class="nav6">Contact Us</a></li>
+        <li class="divider"></li>
+        <li><a href="aboutus.html" class="nav5">About Us</a></li>
+        <li class="divider"></li>
+      </ul>
+      <div class="right_menu_corner"></div>
+    </div>
+    <!-- end of menu tab -->
+    <div class="crumb_navigation"> Navigation: <span class="current">Home</span> </div>
+    <div class="left_content">
+      <div class="title_box">Categories</div>
+      <ul class="left_menu">
+        <li class="odd"><a href="category.html">Accessories</a></li>
+        <li class="even"><a href="category.html">Novels </a></li>
+        <li class="odd"><a href="category.html">Gifts</a></li>
+        <li class="even"><a href="category.html">Home Decor & Furnishing</a></li>
+        <li class="odd"><a href="category.html">Kitchen Utilities & Appliances</a></li>
+        <li class="even"><a href="category.html">Mobile Phone & Accesories</a></li>
+        <li class="odd"><a href="category.html">Personal Care & Beauty</a></li>
+        <li class="even"><a href="category.html">Toys & Games</a></li>
+        <li class="odd"><a href="category.html">Travel & Accessories</a></li>
+        <li class="even"><a href="category.html">Video& Music</a></li>
+        <li class="odd"><a href="category.html">Watches</a></li>
+      </ul>
+      <div class="title_box">Special Products</div>
+      <div class="border_box">
+        <div class="product_title"><a href="details.html">Motorola 156 MX-VL</a></div>
+        <div class="product_img"><a href="details.html"><img src="images/laptop.png" alt="" border="0" /></a></div>
+        <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span></div>
+      </div>
+      
+      
+    </div>
+    <!-- end of left content -->
+     <div class="center_content">
+      <form method="POST" action="formsubmit.php">
+		<h1><center> Registeration </center></h1><br>
+		<table border="1" align="center">
+		<tr>
+		<td> <label class="contact"><strong>User Name:</strong></label>
+		<td><input type="text" name="name">
+		<tr>
+		<td><label class="contact"><strong>Password:</strong></label>
+		<td><input type="password" name="pwd">
+		<tr>
+		<td><label class="contact"><strong>Birthday:</strong></label>
+		<td><input type="text" name="birthday">
+		<tr>
+		<td rowspan="2"><label class="contact"><strong>Gender:</strong></label>
+		<td><input type="radio" name="sex" value="male"> <label class="contact"><strong>MALE</strong></label>
+		<tr>
+		<td><input type="radio" name="sex" value="female"><label class="contact"><strong>FEMALE</strong></label>
+		<tr>
+		<td><label class="contact"><strong>Address:</strong></label>
+		<td><input type="text" name="address">
+		<tr>
+		<td><label class="contact"><strong>Contact No:</strong></label>
+		<td><input type="tel" name="contact">
+		<tr>
+		<td><label class="contact"><strong>E-mail:</strong></label>
+		<td><input type="email" name="email">
+		<tr>
+		<td colspan="2"><center> <input type="submit" value="Submit" name="submit"> </center>
+		</table>
+		</form>
+    </div>
+    <!-- end of center content -->
+    
+       <div class="right_content">
+      <div class="shopping_cart">
+        <div class="cart_title">Shopping cart</div>
+        <div class="cart_details"> 3 items <br />
+          <span class="border_cart"></span> Total: <span class="price">350$</span> </div>
+        <div class="cart_icon"><a href="http://all-free-download.com/free-website-templates/" title="header=[Checkout] body=[&nbsp;] fade=[on]"><img src="images/shoppingcart.png" alt="" width="48" height="48" border="0" /></a></div>
+      </div>
+      <div class="title_box">What's new</div>
+      <div class="border_box">
+        <div class="product_title"><a href="details.html">Motorola 156 MX-VL</a></div>
+        <div class="product_img"><a href="details.html"><img src="images/p2.gif" alt="" border="0" /></a></div>
+        <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span></div>
+      </div>
+    
+    <!-- end of right content -->
+  </div>
+  <!-- end of main content -->
+  <div class="footer">
+       <div class="center_footer">  All Rights Reserved 
+       </div>
+  </div>
+</div>
+<!-- end of main_container -->
+</body>
+</html>
